869e9dd4429c03d160302410b4a9492042d5caf9
stepan <spelinichenko15@gmail.com>
2023-04-17 19:45:26 +0300

    init

---
+++ src/main/java/com/example/servingwebcontent/FirstJavaSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstJavaSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/servingwebcontent/FirstJavaSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
