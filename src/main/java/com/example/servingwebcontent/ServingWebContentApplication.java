e8296ef888621919562a854fe31f44a2116209f4
stepan <spelinichenko15@gmail.com>
2023-04-17 20:01:45 +0300

    added new html file and file with main function

--- src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstJavaSpringProjectApplication {
+public class ServingWebContentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FirstJavaSpringProjectApplication.class, args);
+        SpringApplication.run(ServingWebContentApplication.class, args);
     }
 
 }
